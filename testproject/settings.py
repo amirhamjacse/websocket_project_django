457d31c564aad1eb50ba7c29c87c30a374a5d099
amirhamjacse <amirhamza824@gmail.com>
2024-11-26 14:55:38 +0600

    setting and urls

--- testproject/settings.py
+++ testproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ud9g#!mn^pb%z%&jx&56hl5vuw0t^v-80chi#4z1)*00hs(&rc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
